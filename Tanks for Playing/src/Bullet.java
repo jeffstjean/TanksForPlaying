bd52d0ef77852d4a40ee351408bc66839aef071c
jeffstjean <jeffrey@stjean.ca>
2017-10-17 09:44:53 -0400

    Started Bullet Class

--- Tanks for Playing/src/Bullet.java
+++ Tanks for Playing/src/Bullet.java
@@ -1,21 +1,33 @@
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
+public class Bullet extends GameObject {
 
-public class Bullet extends GameObject{
+    private Rectangle bounds;
+    private final int size = 64;
+    private final int speed = 2;
+    private Game game;
+    private BufferedImage body;
+    private double rotate;
 
-    public Bullet(int x, int y, ID id) {
+    public Bullet(int x, int y, ID id, Game g) {
         super(x, y, id);
+        bounds = new Rectangle(x, y, size, size);
+        game = g;
+        rotate = 0;
+
     }
 
     @Override
     public void tick() {
-        
+
     }
 
     @Override
     public void render(Graphics g) {
-        
+
     }
-    
+
 }
