139c216b822e18b00e4a92c5308d1f06378ef37b
jeffstjean <jeffrey@stjean.ca>
2017-10-17 10:06:18 -0400

    Rendered Bullet

--- Tanks for Playing/src/Bullet.java
+++ Tanks for Playing/src/Bullet.java
@@ -1,9 +1,12 @@
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 
-public class Bullet extends GameObject {
+public class Bullet extends GameObject implements MouseListener {
 
     private Rectangle bounds;
     private final int size = 64;
@@ -17,7 +20,12 @@ public class Bullet extends GameObject {
         bounds = new Rectangle(x, y, size, size);
         game = g;
         rotate = 0;
+        body = ImageLoader.imageLoader("./graphics/Bullet.png");
 
+    }
+    
+    public Game getGame() {
+        return game;
     }
 
     @Override
@@ -25,9 +33,42 @@ public class Bullet extends GameObject {
 
     }
 
+
     @Override
     public void render(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.draw(bounds);
+        
+        g2d.drawImage(body, x, y, size, size, null);
+    }
+    
+    public int getSize() {
+        return size;
+    }
 
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        System.out.println("Mouse pressed");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
