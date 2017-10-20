25d799f1e675cfe7c561cde6a801094abcefe357
owen7900 <owen7900@scdsb.on.ca>
2017-10-20 15:27:59 -0400

    enemy turret works

---
+++ TFP-Client/src/ClientRecieveThread.java
@@ -0,0 +1,16 @@
+
+import java.net.DatagramSocket;
+
+
+public class ClientRecieveThread extends Thread{
+    private DatagramSocket socket;
+    
+    public ClientRecieveThread(){
+        
+    }
+    
+    @Override
+    public void run(){
+        
+    }
+}
