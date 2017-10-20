8546df83b7cf1f87b1533568fbbef1381513633c
owen7900 <owen7900@scdsb.on.ca>
2017-10-20 15:47:55 -0400

    multiplayer with multithreading

--- TFP-Client/src/ClientRecieveThread.java
+++ TFP-Client/src/ClientRecieveThread.java
@@ -1,16 +1,45 @@
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 
 public class ClientRecieveThread extends Thread{
     private DatagramSocket socket;
-    
-    public ClientRecieveThread(){
-        
+    private String [] host = {"192.168.0.15"};//jeff 99.233.204.138 owen 192.168.0.10
+   private InetAddress address;
+   private DatagramPacket packet;
+   private final int PORT = 4447;
+    private Game game;
+    private byte[] buf = new byte[256];
+    public ClientRecieveThread(Game g){
+        game = g;
+        try{
+            socket = new DatagramSocket();
+            address = InetAddress.getByName(host[0]);
+            packet = new DatagramPacket(game.getAllBytes(), game.getAllBytes().length, address, PORT);
+        }catch (Exception e){
+            System.out.println("shit server error");
+        }
     }
     
     @Override
     public void run(){
-        
+        while(true){
+        packet = new DatagramPacket(game.getAllBytes(), game.getAllBytes().length, address, PORT);
+        try {
+            
+            socket.send(packet);
+            System.out.println("sent");
+            packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            System.out.println("recived");
+            game.decodeBytes(packet.getData());
+        } catch (IOException ex) {
+            System.out.println("servererrorspot1");
+        }
     }
+}
+
 }
