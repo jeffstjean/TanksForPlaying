dbb297b24614a15607737a4684df51ba1e7a196e
owen7900 <owen7900@scdsb.on.ca>
2017-10-25 18:49:04 -0400

    Server is running full instance of game

--- TFP-Server/src/Key.java
+++ TFP-Server/src/Key.java
@@ -5,7 +5,16 @@ public class Key{
    public Key left = new Key();
    public Key right = new Key();
    public Key shoot = new Key();
- ;
+
+   
+   
+   public Key(){
+   up = new Key();
+   down = new Key();
+   left = new Key();
+   right = new Key();
+   shoot = new Key();
+}
 
    /* toggles the keys current state*/
    public void toggle(){
