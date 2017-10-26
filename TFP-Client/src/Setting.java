471896bfc7952c4bc883f8537b57e9c0720fb1ff
jeffstjean <31709307+jeffstjean@users.noreply.github.com>
2017-10-26 15:14:40 -0400

    Add files via upload

---
+++ TFP-Client/src/Setting.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jeff4821
+ */
+public enum Setting {
+    NUM_PLAYERS, PORT, FPS, IP, TANK_SIZE, TANK_SPEED, BULLET_SIZE, BULLET_SPEED, MAP_LAYOUT
+}
---
+++ TFP-Client/src/SettingsManager.java
@@ -0,0 +1,118 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.LinkedList;
+
+public class SettingsManager {
+	static File config = new File("src/resources/configs/config.txt");
+	static File blankConfig = new File("src/resources/default_configs/default_config.txt");
+	static String line = null;
+	static LinkedList<String> settings = new LinkedList<String>();
+	static boolean isError = false;
+
+	public static void init() {
+		try {
+			if (!config.exists()) {
+				config.createNewFile(); 
+				Files.copy(blankConfig.toPath(), config.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			}
+			FileReader fr = new FileReader(config);
+			BufferedReader br = new BufferedReader(fr);
+
+			while ((line = br.readLine()) != null) {
+				String words[] = line.split(": ");
+				if (words.length != 2) {
+					// Settings aren't written properly
+					isError = true;
+					break;
+				}
+				settings.add(words[1]);
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("FILE NOT FOUND EXCEPTION");
+		} catch (IOException e) {
+			System.err.println("IO EXCEPTION");
+		}
+		if (isError) {
+			System.err.println("Settings aren't configured properly");
+			return;
+		}
+
+		for (int i = 0; i < settings.size(); i++) {
+		}
+	}
+
+	public static int getIntSetting(Setting set) {
+		switch (set) {
+		case NUM_PLAYERS:
+			try {
+				return Integer.parseInt(settings.get(0));
+			} catch (Exception e) {
+				return -1;
+			}
+		case PORT:
+			try {
+				return Integer.parseInt(settings.get(1));
+			} catch (Exception e) {
+				return -1;
+			}
+		case FPS:
+			try {
+				return Integer.parseInt(settings.get(2));
+			} catch (Exception e) {
+				return -1;
+			}
+		case TANK_SIZE:
+			try {
+				return Integer.parseInt(settings.get(4));
+			} catch (Exception e) {
+				return -1;
+			}
+		case TANK_SPEED:
+			try {
+				return Integer.parseInt(settings.get(5));
+			} catch (Exception e) {
+				return -1;
+			}
+		case BULLET_SIZE:
+			try {
+				return Integer.parseInt(settings.get(6));
+			} catch (Exception e) {
+				return -1;
+			}
+		case BULLET_SPEED:
+			try {
+				return Integer.parseInt(settings.get(7));
+			} catch (Exception e) {
+				return -1;
+			}
+		case MAP_LAYOUT:
+			try {
+				return Integer.parseInt(settings.get(8));
+			} catch (Exception e) {
+				return -1;
+			}
+		default: 
+			return -1;
+		}
+	}
+
+	public static String getStringSetting(Setting set) {
+		switch (set) {
+		case IP:
+			try {
+				return settings.get(3);
+			} catch (Exception e) {
+				
+			}
+		default:
+			return null;
+		}
+	}
+}
