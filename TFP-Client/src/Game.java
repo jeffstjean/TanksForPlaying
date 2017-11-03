55e2f258e084d0d7a0b28a72fcb9e77923940cab
owen7900 <owen7900@scdsb.on.ca>
2017-11-03 10:29:46 -0400

    commented the encoder/decoder

--- TFP-Client/src/Game.java
+++ TFP-Client/src/Game.java
@@ -432,39 +432,53 @@ public class Game implements Runnable, KeyListener, MouseInputListener {
 
     private void createBytes() {
         allBytes[0] = 1; // says its an in game byte
-
+        
+        //sets the up byte to 1 or 0 
+        //0 is pressed 1 is not pressed
         if (Key.up.isDown) {
             allBytes[1] = 0;
         } else {
             allBytes[1] = 1;
         }
 
+        
+        //sets the down byte to 1 or 0 
+        //0 is pressed 1 is not pressed
         if (Key.down.isDown) {
             allBytes[2] = 0;
         } else {
             allBytes[2] = 1;
         }
 
+        
+        //sets the left byte to 1 or 0 
+        //0 is pressed 1 is not pressed
         if (Key.left.isDown) {
             allBytes[3] = 0;
         } else {
             allBytes[3] = 1;
         }
 
+        
+        //sets the right byte to 1 or 0 
+        //0 is pressed 1 is not pressed
         if (Key.right.isDown) {
             allBytes[4] = 0;
         } else {
             allBytes[4] = 1;
         }
 
+        
         //the x encoder start
-        bb = ByteBuffer.allocate(4);
-        bb.putInt(mouseX);
-        byte[] temp = bb.array();
+        bb = ByteBuffer.allocate(4); // byte buffer becomes 4 bytes long
+        bb.putInt(mouseX); // puts the int in the buffer
+        byte[] temp = bb.array(); // sets the array temp to equal the buffer
         for (int i = 0; i < temp.length; i++) {
-            allBytes[i + 5] = temp[i];
+            allBytes[i + 5] = temp[i]; // sets the selected allBytes values to the correct bytes
         }
         // the x encoder end
+        
+        
         // the y encoder start
         bb = ByteBuffer.allocate(4);
         bb.putInt(mouseY);
@@ -497,49 +511,64 @@ public class Game implements Runnable, KeyListener, MouseInputListener {
 
     public void decodeBytes(byte[] bmain) {
         System.out.println("recieved");
-        byte[] temp = new byte[8];
-        if (bmain[0] == 1) {
+        
+        //decodes the time that this packet was originally sent
+        byte[] temp = new byte[8]; // inits the temp byte[] as an 8 byte array
+        if (bmain[0] == 1) { // only runs this if the packet is a real packet not a blank packet
             for (int i = 0; i < 8; i++) {
-                temp[i] = bmain[i + 240];
+                temp[i] = bmain[i + 240]; // sets the byes in temp to be the 8 bytes i want decoded
             }
 
-            bb = ByteBuffer.wrap(temp);
-            long tempL = bb.getLong();
-            if (/*maxMillis < tempL*/ true ) { //this is commented out for debug purposes
-                maxMillis = tempL;
+            bb = ByteBuffer.wrap(temp);//gives he buffer temp[] to read from 
+            long tempL = bb.getLong(); // reads the long value to a tempL variable
+            
+            if (/*maxMillis < tempL*/ true ) {//only uses the packet if it is a new packet //this is commented out for debug purposes
+                maxMillis = tempL; // sets the most recent packet index to the current packts index number
 
-                for (int j = 0; j < NUM_PLAYERS; j++) {
-
-                    temp = new byte[4];
+                for (int j = 0; j < NUM_PLAYERS; j++) { // decodes the basic player info the number of times as there are players
+                    //x decoder start
+                    temp = new byte[4]; // re-sets the length of temp to 4
                     for (int i = 0; i < 4; i++) {
-                        temp[i] = bmain[i + 1 + (20 * j)];
+                        temp[i] = bmain[i + 1 + (20 * j)]; // sets temp to be the 4 bytes that represent the x location of tank[j]
                     }
-                    bb = ByteBuffer.wrap(temp);
-                    tank[j].setX(bb.getInt());
-
+                    bb = ByteBuffer.wrap(temp); // gives the buffer the temp
+                    tank[j].setX(bb.getInt()); // sets tank[j]'s location
+                    // x decoder end
+                    
+                    //y decoder start, please see x decoder for reference
                     temp = new byte[4];
                     for (int i = 0; i < 4; i++) {
                         temp[i] = bmain[i + 5 + (20 * j)];
                     }
                     bb = ByteBuffer.wrap(temp);
                     tank[j].setY(bb.getInt());
-
-                    temp = new byte[8];
+                    //y decoder end
+                    
+                    //rotation encoder start, please see x encoder for reference
+                    temp = new byte[8]; 
                     for (int i = 0; i < 8; i++) {
                         temp[i] = bmain[i + 10 + (20 * j)];
                     }
                     bb = ByteBuffer.wrap(temp);
-                    turret[j].setRotate(bb.getDouble());
+                    turret[j].setRotate(bb.getDouble()); // sets the turrets pointing to the pointing specified by the server
+                    //rotation decoder end
+                    
+                    
+                    // shooting decoder start
                     if (bmain[19 + (20 * j)] == 0) {
-                        turret[j].shoot();
+                        turret[j].shoot(); // if the shooting byte = 0 it will shoot
                     }
-                    tank[j].setPointing(bmain[20 + (20 * j)]);
-
+                    //shooting encoder end
+                    
+                    
+                    tank[j].setPointing(bmain[20 + (20 * j)]); // sets the pointing enum to the numerical value of where the server is telling the client
+                    
+                    
                     System.out.println("decoded  X:" + tank[j].getX() + "    Y:" + tank[j].getY() + "    R:" + turret[j].getRotate());
                 }
 
             } else {
-                System.out.println("got a bad one");
+                System.out.println("got an old packet"); 
             }
         }
 
